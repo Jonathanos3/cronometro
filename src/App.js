fde96068cf2d896ab6291e973bf1fc2061aadd8d
Jonathan Oliveira <jonathanoliveira2@hotmail.com>
2022-11-23 21:25:13 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: "GO",
+    };
+    this.timer = null;
+    this.go = this.go.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  go() {
+    let state = this.state;
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = "GO";
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.botao = "PAUSE";
+    }
+    this.setState(state);
+  }
+
+  clear() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = "GO";
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img className="img" src={require("./assets/cronometro.png")} />
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
+        <a className="texto">Cronômeto</a>
+        <div className="areaBtn">
+          <a className="botao" onClick={this.go}>
+            {this.state.botao}
+          </a>
+          <a className="botao" onClick={this.clear}>
+            CLEAR
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
